1932db77b113583c0749e131b74ff9e289f48dbf
HN <harrynoimark@gmail.com>
2026-06-29 12:01:28 +0000

    new changes

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end(fs.readFileSync(__dirname + '/index.html'));
+    } else if (req.url.endsWith('.css')) {
+        res.end(fs.readFileSync(__dirname + req.url));
+    } else if (req.url === '/register' && req.method === 'POST') {
+        res.end('<h1>Booking confirmed!</h1><a href="/">Home</a>');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end('Page not found');
+    }
+}).listen(3000);
